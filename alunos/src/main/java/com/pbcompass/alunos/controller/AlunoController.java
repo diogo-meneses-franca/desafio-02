9102f441b4edd3c1b627fcdae7c4599192952c0a
Nicolas <nicolas.marsil02@gmail.com>
2024-05-07 10:35:51 -0300

    feat: método cadastrar para Aluno

--- alunos/src/main/java/com/pbcompass/alunos/controller/AlunoController.java
+++ alunos/src/main/java/com/pbcompass/alunos/controller/AlunoController.java
@@ -1,7 +1,15 @@
 package com.pbcompass.alunos.controller;
 
+import com.pbcompass.alunos.dto.AlunoCriarDto;
+import com.pbcompass.alunos.dto.AlunoRespostaDto;
+import com.pbcompass.alunos.entity.Aluno;
+import com.pbcompass.alunos.mapper.AlunoMapper;
 import com.pbcompass.alunos.service.AlunoService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlunoController {
 
     private final AlunoService alunoService;
+
+    @PostMapping
+    public ResponseEntity<AlunoRespostaDto> cadastrar(@RequestBody @Valid AlunoCriarDto dto) {
+        Aluno aluno = AlunoMapper.toAluno(dto);
+        AlunoRespostaDto resposta = AlunoMapper.toRespostaDto(alunoService.cadastrar(aluno));
+        return ResponseEntity.ok(resposta);
+    }
+
 }
--- alunos/src/main/java/com/pbcompass/alunos/service/AlunoService.java
+++ alunos/src/main/java/com/pbcompass/alunos/service/AlunoService.java
@@ -1,7 +1,11 @@
 package com.pbcompass.alunos.service;
 
+import com.pbcompass.alunos.entity.Aluno;
+import com.pbcompass.alunos.exception.CpfUniqueViolationException;
 import com.pbcompass.alunos.repository.AlunoRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,5 +13,14 @@ import org.springframework.stereotype.Service;
 public class AlunoService {
 
     private final AlunoRepository alunoRepository;
+
+    @Transactional
+    public Aluno cadastrar(Aluno aluno) {
+        try {
+            return alunoRepository.save(aluno);
+        } catch (DataIntegrityViolationException e) {
+            throw new CpfUniqueViolationException(String.format("CPF '%s' já cadastrado", aluno.getCpf()));
+        }
+    }
 
 }
