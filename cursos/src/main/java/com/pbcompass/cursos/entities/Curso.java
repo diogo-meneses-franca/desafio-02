5a38054357f29a94aa4d33647814edf1023cf308
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-05-07 10:25:00 -0300

    feature - Adicionado classe de entidade Curso

---
+++ cursos/src/main/java/com/pbcompass/cursos/entities/Curso.java
@@ -0,0 +1,62 @@
+package com.pbcompass.cursos.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cursos")
+@Entity
+public class Curso implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "quantidadeHoras", nullable = false)
+    private Integer quantidadeHoras;
+
+    @ManyToOne
+    @JoinColumn(name = "professorId")
+    private Professor professor;
+
+
+    @Column(name = "ativo", nullable = false)
+    private boolean ativo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "area", length = 25)
+    private Area area = Area.NULL;
+
+    public enum Area{
+        CIENCIAS_HUMANAS,
+        CIENCIAS_SOCIAIS,
+        TECNOLOGIAS,
+        NULL
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Curso curso = (Curso) o;
+        return Objects.equals(id, curso.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
