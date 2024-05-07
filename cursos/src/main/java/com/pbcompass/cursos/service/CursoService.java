a9e69cced58d6649452c2daa3687892d11249fdd
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-05-07 17:44:23 -0300

    feat: ProfessorService

--- cursos/src/main/java/com/pbcompass/cursos/service/CursoService.java
+++ cursos/src/main/java/com/pbcompass/cursos/service/CursoService.java
@@ -10,8 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @RequiredArgsConstructor
 @Service
 public class CursoService {
