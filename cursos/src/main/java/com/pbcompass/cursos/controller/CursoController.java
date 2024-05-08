150d059a1aa247399b5abd54588de68599f95d44
tomazdalcortivo <p12547254@gmail.com>
2024-05-08 19:43:50 -0300

    feat: método cadastrar

--- cursos/src/main/java/com/pbcompass/cursos/controller/CursoController.java
+++ cursos/src/main/java/com/pbcompass/cursos/controller/CursoController.java
@@ -1,9 +1,14 @@
 package com.pbcompass.cursos.controller;
 
+import com.pbcompass.cursos.dto.CursoCriarDto;
+import com.pbcompass.cursos.dto.CursoRespostaDto;
+import com.pbcompass.cursos.dto.mapper.CursoMapper;
+import com.pbcompass.cursos.entities.Curso;
 import com.pbcompass.cursos.service.CursoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -12,5 +17,11 @@ public class CursoController {
 
     private final CursoService service;
 
+    @PostMapping
+    public ResponseEntity<CursoRespostaDto> cadastrar(@RequestBody CursoCriarDto dto){
+        Curso curso = CursoMapper.toCurso(dto);
+        CursoRespostaDto respostaDto = CursoMapper.toRespostaDto(service.cadastrar(curso));
+        return ResponseEntity.status(HttpStatus.CREATED).body(respostaDto);
+    }
 
 }
