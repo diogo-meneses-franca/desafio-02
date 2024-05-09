d2cfade7a69c56dd294bf3251e3918489e6d9da2
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-05-09 16:10:44 -0300

    feat: toListaProfessor

--- cursos/src/main/java/com/pbcompass/cursos/dto/mapper/ProfessorMapper.java
+++ cursos/src/main/java/com/pbcompass/cursos/dto/mapper/ProfessorMapper.java
@@ -7,6 +7,9 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProfessorMapper {
 
@@ -16,5 +19,11 @@ public class ProfessorMapper {
 
     public static ProfessorRespostaDto toRespostaDto(Professor professor){
         return new ModelMapper().map(professor, ProfessorRespostaDto.class);
+    }
+
+    public static List<ProfessorRespostaDto> toListaDto(List<Professor> profList) {
+        return profList.stream()
+                .map(ProfessorMapper::toRespostaDto)
+                .collect(Collectors.toList());
     }
 }
