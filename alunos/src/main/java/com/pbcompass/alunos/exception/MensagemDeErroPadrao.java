f8e29a32b980bebdc1f028d1c9dc781388c8c524
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-05-09 17:02:01 -0300

    refactor: removido hashmap erros e construtor na classe MensagemDeErroPadrao

--- alunos/src/main/java/com/pbcompass/alunos/exception/MensagemDeErroPadrao.java
+++ alunos/src/main/java/com/pbcompass/alunos/exception/MensagemDeErroPadrao.java
@@ -5,11 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @NoArgsConstructor
 @Getter
@@ -22,28 +17,11 @@ public class MensagemDeErroPadrao {
     private String statusMensagem;
     private String mensagem;
 
-    private Map<String, String> erros = new HashMap<>();
-
     public MensagemDeErroPadrao(HttpServletRequest request, HttpStatus status, String mensagem) {
         this.path = request.getRequestURI();
         this.method = request.getMethod();
         this.status = status.value();
         this.statusMensagem = status.getReasonPhrase();
         this.mensagem = mensagem;
-    }
-
-    public MensagemDeErroPadrao(HttpServletRequest request, HttpStatus status, String mensagem, BindingResult result) {
-        this.path = request.getRequestURI();
-        this.method = request.getMethod();
-        this.status = status.value();
-        this.statusMensagem = status.getReasonPhrase();
-        this.mensagem = mensagem;
-        addErrors(result);
-    }
-
-    private void addErrors(BindingResult result) {
-        for(FieldError error : result.getFieldErrors()) {
-            erros.put(error.getField(), error.getDefaultMessage());
-        }
     }
 }
