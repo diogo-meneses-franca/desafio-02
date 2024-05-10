2cb5a78d3f8d0b5897446ae04d2626d3d875c0f8
tomazdalcortivo <p12547254@gmail.com>
2024-05-10 19:02:33 -0300

    docs: cadrastar professor

--- cursos/src/main/java/com/pbcompass/cursos/controller/ProfessorController.java
+++ cursos/src/main/java/com/pbcompass/cursos/controller/ProfessorController.java
@@ -1,10 +1,17 @@
 package com.pbcompass.cursos.controller;
 
+import com.pbcompass.cursos.dto.CursoRespostaDto;
 import com.pbcompass.cursos.dto.ProfessorCadastrarDto;
 import com.pbcompass.cursos.dto.ProfessorRespostaDto;
 import com.pbcompass.cursos.dto.mapper.ProfessorMapper;
 import com.pbcompass.cursos.entities.Professor;
+import com.pbcompass.cursos.exceptions.ErrorMessage;
 import com.pbcompass.cursos.service.ProfessorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Professor", description = "Contém todas as operações relativas aos recursos para cadastro e leitura de um Professor.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/professores")
@@ -19,6 +27,18 @@ public class ProfessorController {
 
     private final ProfessorService service;
 
+    @Operation(summary = "Cadastrar um novo Professor",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Professor cadastrar com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CursoRespostaDto.class))),
+                    @ApiResponse(
+                            responseCode = "422",
+                            description = "Dados de entrada inválidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class))),
+            }
+    )
     @PostMapping
     public ResponseEntity<ProfessorRespostaDto> cadastrar(@RequestBody ProfessorCadastrarDto dto) {
         Professor professor = ProfessorMapper.toProfessor(dto);
