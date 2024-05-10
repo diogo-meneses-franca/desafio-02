893968d618c97543ec3ffdbb2abffbac62c7bd20
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-05-10 12:05:41 -0300

    feat: methodArgumentNotValidException e PersistenceException

--- cursos/src/main/java/com/pbcompass/cursos/exceptions/customizadas/ApiExceptionHandler.java
+++ cursos/src/main/java/com/pbcompass/cursos/exceptions/customizadas/ApiExceptionHandler.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -18,5 +20,21 @@ public class ApiExceptionHandler {
         log.error("Api error ", e);
         return ResponseEntity.status(HttpStatus.NOT_FOUND).contentType(MediaType.APPLICATION_JSON)
                 .body(new ErrorMessage(request, HttpStatus.NOT_FOUND, e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorMessage> methodArgumentNotValidException(MethodArgumentNotValidException e,
+                                                                        HttpServletRequest request, BindingResult result){
+        log.error("Api error ", e);
+        return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.UNPROCESSABLE_ENTITY, "Campo inválido", result));
+    }
+
+    @ExceptionHandler(PersistenceException.class)
+    public ResponseEntity<ErrorMessage> persistenceException(PersistenceException e,
+                                                             HttpServletRequest request, BindingResult result){
+        log.error("Api error ", e);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).contentType(MediaType.APPLICATION_JSON)
+                .body(new ErrorMessage(request, HttpStatus.BAD_REQUEST, "Erro ao salvar", result));
     }
 }
