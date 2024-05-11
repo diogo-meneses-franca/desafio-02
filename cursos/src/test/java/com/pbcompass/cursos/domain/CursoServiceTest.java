1d2fa0d0239c57d155f707900ab2d4c19148ba75
Nicolas <nicolas.marsil02@gmail.com>
2024-05-10 23:12:52 -0300

    refactor: imports desnecessarios apagados

--- cursos/src/test/java/com/pbcompass/cursos/domain/CursoServiceTest.java
+++ cursos/src/test/java/com/pbcompass/cursos/domain/CursoServiceTest.java
@@ -11,13 +11,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.List;
 import java.util.Optional;
 
 import static com.pbcompass.cursos.common.CursoConstantes.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
