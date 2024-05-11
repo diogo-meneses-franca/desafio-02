0e61427f73ddcaf152bf170fbb7ecf8a911b83d0
Nicolas <nicolas.marsil02@gmail.com>
2024-05-11 00:59:07 -0300

    feat: teste metodo toProfessor em ProfessorMapper

--- cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorMapperTest.java
+++ cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorMapperTest.java
@@ -1,10 +1,19 @@
 package com.pbcompass.cursos.domain;
 
+import com.pbcompass.cursos.dto.mapper.ProfessorMapper;
+import com.pbcompass.cursos.entities.Professor;
 import org.junit.jupiter.api.Test;
+
+import static com.pbcompass.cursos.common.ProfessorMapperConstantes.PROFESSOR_CADASTRAR_DTO;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProfessorMapperTest {
 
     @Test
-    public void toProfessor_RetornarProfessor() {}
+    public void toProfessor_RetornarProfessor() {
+        Professor testeProf = ProfessorMapper.toProfessor(PROFESSOR_CADASTRAR_DTO);
+
+        assertThat(testeProf.getNome()).isEqualTo(PROFESSOR_CADASTRAR_DTO.getNome());
+    }
 
 }
