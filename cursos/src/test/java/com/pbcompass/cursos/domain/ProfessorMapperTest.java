43cd798d5cca0471d27aacef6d3578f795131bb7
Nicolas <nicolas.marsil02@gmail.com>
2024-05-11 00:51:43 -0300

    feat: classe ProfessorMapperTest

---
+++ cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorMapperTest.java
@@ -0,0 +1,10 @@
+package com.pbcompass.cursos.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class ProfessorMapperTest {
+
+    @Test
+    public void toProfessor_RetornarProfessor() {}
+
+}
