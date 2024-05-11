25bf10246f6349f9e1072cd5abc851832fc12edb
Nicolas <nicolas.marsil02@gmail.com>
2024-05-10 23:26:48 -0300

    feat: testes metodo cadastrar em ProfessorService

--- cursos/src/test/java/com/pbcompass/cursos/common/ProfessorConstantes.java
+++ cursos/src/test/java/com/pbcompass/cursos/common/ProfessorConstantes.java
@@ -5,5 +5,6 @@ import com.pbcompass.cursos.entities.Professor;
 public class ProfessorConstantes {
 
     public static final Professor PROFESSOR = new Professor(1L, "Marcos");
+    public static final Professor PROFESSOR_INVALIDO = new Professor(null, null);
 
 }
--- cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorServiceTest.java
+++ cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorServiceTest.java
@@ -1,11 +1,20 @@
 package com.pbcompass.cursos.domain;
 
+import com.pbcompass.cursos.entities.Professor;
+import com.pbcompass.cursos.exceptions.customizadas.PersistenceException;
 import com.pbcompass.cursos.repository.ProfessorRepository;
 import com.pbcompass.cursos.service.ProfessorService;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.pbcompass.cursos.common.ProfessorConstantes.PROFESSOR;
+import static com.pbcompass.cursos.common.ProfessorConstantes.PROFESSOR_INVALIDO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ProfessorServiceTest {
@@ -15,5 +24,20 @@ public class ProfessorServiceTest {
 
     @Mock
     private ProfessorRepository professorRepository;
+
+    @Test
+    public void cadastrarCurso_ComDadosValidos_RetornarCurso() {
+        when(professorRepository.save(PROFESSOR)).thenReturn(PROFESSOR);
+        Professor testeProf = professorService.cadastrar(PROFESSOR);
+
+        assertThat(testeProf).isEqualTo(PROFESSOR);
+    }
+
+    @Test
+    public void cadastrarCurso_ComDadosInvalidos_LancarExcecao() {
+        when(professorRepository.save(PROFESSOR_INVALIDO)).thenThrow(PersistenceException.class);
+
+        assertThatThrownBy(() -> professorService.cadastrar(PROFESSOR_INVALIDO)).isInstanceOf(PersistenceException.class);
+    }
 
 }
