7f919142345e51177eff8f7d4cf29f66997fc086
Nicolas <nicolas.marsil02@gmail.com>
2024-05-10 22:20:49 -0300

    feat: classe CursoConstantes

---
+++ cursos/src/test/java/com/pbcompass/cursos/common/CursoConstantes.java
@@ -0,0 +1,9 @@
+package com.pbcompass.cursos.common;
+
+import com.pbcompass.cursos.entities.Curso;
+
+public class CursoConstantes {
+
+    public static final Curso CURSO = new Curso(1L, "TADS", 20, null, true, Curso.Area.TECNOLOGIAS);
+    
+}
