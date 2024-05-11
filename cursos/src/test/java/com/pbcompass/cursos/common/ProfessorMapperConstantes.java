c165bb64f0aed2a90e105aae61e6501d368b59b3
Nicolas <nicolas.marsil02@gmail.com>
2024-05-11 00:53:34 -0300

    feat: classe ProfessorMapperConstantes

---
+++ cursos/src/test/java/com/pbcompass/cursos/common/ProfessorMapperConstantes.java
@@ -0,0 +1,9 @@
+package com.pbcompass.cursos.common;
+
+import com.pbcompass.cursos.dto.ProfessorCadastrarDto;
+
+public class ProfessorMapperConstantes {
+
+    public static final ProfessorCadastrarDto PROFESSOR_CADASTRAR_DTO = new ProfessorCadastrarDto("PROFESSOR_CADASTRAR_DTO");
+
+}
