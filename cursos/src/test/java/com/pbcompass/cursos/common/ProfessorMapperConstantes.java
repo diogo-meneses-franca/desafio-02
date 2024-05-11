2186b462dcbec7cfdbdc1c5b94e6158d7c64f789
Nicolas <nicolas.marsil02@gmail.com>
2024-05-11 01:02:57 -0300

    feat: teste metodo toRespostaDto em ProfessorMapper

--- cursos/src/test/java/com/pbcompass/cursos/common/ProfessorMapperConstantes.java
+++ cursos/src/test/java/com/pbcompass/cursos/common/ProfessorMapperConstantes.java
@@ -1,9 +1,12 @@
 package com.pbcompass.cursos.common;
 
 import com.pbcompass.cursos.dto.ProfessorCadastrarDto;
+import com.pbcompass.cursos.entities.Professor;
 
 public class ProfessorMapperConstantes {
 
     public static final ProfessorCadastrarDto PROFESSOR_CADASTRAR_DTO = new ProfessorCadastrarDto("PROFESSOR_CADASTRAR_DTO");
+
+    public static final Professor PROFESSOR = new Professor(1L, "PROFESSOR");
 
 }
--- cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorMapperTest.java
+++ cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorMapperTest.java
@@ -1,9 +1,11 @@
 package com.pbcompass.cursos.domain;
 
+import com.pbcompass.cursos.dto.ProfessorRespostaDto;
 import com.pbcompass.cursos.dto.mapper.ProfessorMapper;
 import com.pbcompass.cursos.entities.Professor;
 import org.junit.jupiter.api.Test;
 
+import static com.pbcompass.cursos.common.ProfessorMapperConstantes.PROFESSOR;
 import static com.pbcompass.cursos.common.ProfessorMapperConstantes.PROFESSOR_CADASTRAR_DTO;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +16,14 @@ public class ProfessorMapperTest {
         Professor testeProf = ProfessorMapper.toProfessor(PROFESSOR_CADASTRAR_DTO);
 
         assertThat(testeProf.getNome()).isEqualTo(PROFESSOR_CADASTRAR_DTO.getNome());
+    }
+
+    @Test
+    public void toProfessorRespostaDto_RetornarProfessorRespostaDto() {
+        ProfessorRespostaDto testeProfDto = ProfessorMapper.toRespostaDto(PROFESSOR);
+
+        assertThat(testeProfDto.getId()).isEqualTo(PROFESSOR.getId());
+        assertThat(testeProfDto.getNome()).isEqualTo(PROFESSOR.getNome());
     }
 
 }
