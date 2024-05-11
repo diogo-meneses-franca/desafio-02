ffa648197f000032b3c72b4efbef967d87f7a141
Nicolas <nicolas.marsil02@gmail.com>
2024-05-10 22:27:17 -0300

    feat: testes metodo cadastrar em CursoService

--- cursos/src/test/java/com/pbcompass/cursos/common/CursoConstantes.java
+++ cursos/src/test/java/com/pbcompass/cursos/common/CursoConstantes.java
@@ -5,5 +5,5 @@ import com.pbcompass.cursos.entities.Curso;
 public class CursoConstantes {
 
     public static final Curso CURSO = new Curso(1L, "TADS", 20, null, true, Curso.Area.TECNOLOGIAS);
-    
+
 }
--- cursos/src/test/java/com/pbcompass/cursos/domain/CursoServiceTest.java
+++ cursos/src/test/java/com/pbcompass/cursos/domain/CursoServiceTest.java
@@ -1,5 +1,7 @@
 package com.pbcompass.cursos.domain;
 
+import com.pbcompass.cursos.entities.Curso;
+import com.pbcompass.cursos.exceptions.customizadas.PersistenceException;
 import com.pbcompass.cursos.repository.CursoRepository;
 import com.pbcompass.cursos.service.CursoService;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.pbcompass.cursos.common.CursoConstantes.CURSO;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,5 +23,20 @@ public class CursoServiceTest {
 
     @Mock
     private CursoRepository cursoRepository;
+
+    @Test
+    public void cadastrarCurso_ComDadosValidos_RetornarCurso() {
+        when(cursoRepository.save(CURSO)).thenReturn(CURSO);
+        Curso testeCurso = cursoService.cadastrar(CURSO);
+
+        assertThat(testeCurso).isEqualTo(CURSO);
+    }
+
+    @Test
+    public void cadastrarCurso_ComDadosInvalidos_LancarExcecao() {
+        when(cursoRepository.save(CURSO)).thenThrow(PersistenceException.class);
+
+        assertThatThrownBy(() -> cursoService.cadastrar(CURSO)).isInstanceOf(PersistenceException.class);
+    }
 
 }
