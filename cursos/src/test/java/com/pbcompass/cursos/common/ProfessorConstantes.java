1948435d4ea54fb7e53483572233708741f4e18a
Nicolas <nicolas.marsil02@gmail.com>
2024-05-10 23:21:12 -0300

    feat: classe ProfessorConstantes

---
+++ cursos/src/test/java/com/pbcompass/cursos/common/ProfessorConstantes.java
@@ -0,0 +1,9 @@
+package com.pbcompass.cursos.common;
+
+import com.pbcompass.cursos.entities.Professor;
+
+public class ProfessorConstantes {
+
+    public static final Professor PROFESSOR = new Professor(1L, "Marcos");
+
+}
