5ea3a708c2ac6f8e1c8a4c4fa22f60cd9f445087
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2024-05-12 18:56:44 -0300

    fix: corrigindo metodo cadastrarProfessor_ComDadosInvalidos_lancarExcecao

--- cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorServiceTest.java
+++ cursos/src/test/java/com/pbcompass/cursos/domain/ProfessorServiceTest.java
@@ -1,13 +1,16 @@
 package com.pbcompass.cursos.domain;
 
 import com.pbcompass.cursos.entities.Professor;
+import com.pbcompass.cursos.exceptions.customizadas.DadosDeCadastroInvalidosException;
 import com.pbcompass.cursos.repository.ProfessorRepository;
 import com.pbcompass.cursos.service.ProfessorService;
+import jakarta.persistence.PersistenceException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.util.Optional;
 
@@ -33,14 +36,17 @@ public class ProfessorServiceTest {
 
         assertThat(testeProf).isEqualTo(PROFESSOR);
     }
-    /*
+
     @Test
     public void cadastrarProfessor_ComDadosInvalidos_LancarExcecao() {
-        when(professorRepository.save(PROFESSOR_INVALIDO)).thenThrow(PersistenceException.class);
+        when(professorRepository.save(PROFESSOR_INVALIDO)).thenThrow(DadosDeCadastroInvalidosException.class);
 
-        assertThatThrownBy(() -> professorService.cadastrar(PROFESSOR_INVALIDO)).isInstanceOf(PersistenceException.class);
+        assertThatThrownBy(() -> professorService.cadastrar(PROFESSOR_INVALIDO)).isInstanceOf(DadosDeCadastroInvalidosException.class);
     }
 
+
+
+    /*
     @Test
     public void buscarProfessor_ComIdExistente_RetornarProfessor() {
         when(professorRepository.findById(PROFESSOR.getId())).thenReturn(Optional.of(PROFESSOR));
