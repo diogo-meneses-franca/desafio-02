9159ffc10c5ebe8f3a21b10610cdd09b0a6cb5e5
Diogo Meneses de Franca <m319726@alunos.ipb.pt>
2024-05-15 08:35:47 -0300

    refactor: tratamento de excecao no metodo buscarPorId quando há falha na busca de alunos na api alunos

--- cursos/src/main/java/com/pbcompass/cursos/controller/CursoController.java
+++ cursos/src/main/java/com/pbcompass/cursos/controller/CursoController.java
@@ -1,18 +1,21 @@
 package com.pbcompass.cursos.controller;
 
 import com.pbcompass.cursos.dto.*;
+import com.pbcompass.cursos.exceptions.customizadas.ErroComunicacaoEntreApisException;
 import com.pbcompass.cursos.feignClients.AlunoFeign;
 import com.pbcompass.cursos.dto.mapper.CursoMapper;
 import com.pbcompass.cursos.entities.Aluno;
 import com.pbcompass.cursos.entities.Curso;
 import com.pbcompass.cursos.exceptions.MensagemErroPadrao;
 import com.pbcompass.cursos.service.CursoService;
+import feign.FeignException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +24,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Slf4j
 @Tag(name = "Curso", description = "Contém todas as operações relativas aos recursos para cadastro, edição e leitura de um Curso.")
 @RequiredArgsConstructor
 @RestController
@@ -83,17 +87,26 @@ public class CursoController {
     public ResponseEntity<CursoRespostaDto> buscarPorId(@PathVariable Long id) {
         Curso curso = service.buscarPorId(id);
         Set<Aluno> alunos = curso.getAlunos();
-        List<AlunoFeignBuscarTodosDto> alunosList = alunoFeign.buscarTodos().getBody();
-        Set<AlunoRespostaDto> respostaDtos = new HashSet<>();
-        for(AlunoFeignBuscarTodosDto alunoDto : alunosList) {
-            for(Aluno aluno : alunos) {
-                if(aluno.getId().equals(alunoDto.getId())) {
-                    respostaDtos.add(new AlunoRespostaDto(alunoDto.getNome(), alunoDto.getSexo(), aluno.isAtivo()));
+        if (!alunos.isEmpty()){
+            try{
+                List<AlunoFeignBuscarTodosDto> alunosList = alunoFeign.buscarTodos().getBody();
+                Set<AlunoRespostaDto> respostaDtos = new HashSet<>();
+                for(AlunoFeignBuscarTodosDto alunoDto : alunosList) {
+                    for(Aluno aluno : alunos) {
+                        if(aluno.getId().equals(alunoDto.getId())) {
+                            respostaDtos.add(new AlunoRespostaDto(alunoDto.getNome(), alunoDto.getSexo(), aluno.isAtivo()));
+                        }
+                    }
                 }
+                CursoRespostaDto cursoDto = CursoMapper.toRespostaDto(curso);
+                cursoDto.setAlunos(respostaDtos);
+                return ResponseEntity.ok().body(cursoDto);
+            }catch (FeignException e){
+                log.error(String.format("Erro ao buscar curso por id:  %s", e.getMessage()));
+                throw new ErroComunicacaoEntreApisException("Falha ao buscar alunos matriculados");
             }
         }
         CursoRespostaDto cursoDto = CursoMapper.toRespostaDto(curso);
-        cursoDto.setAlunos(respostaDtos);
         return ResponseEntity.ok().body(cursoDto);
     }
 
